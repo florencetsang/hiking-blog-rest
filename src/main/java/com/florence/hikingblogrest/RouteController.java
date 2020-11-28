a9290a7af47abf3da1cf0beb01d55a3ea933ce1e
florencetsang <florencesytsang@gmail.com>
2020-11-28 22:46:00 +0800

    fetch one route

--- src/main/java/com/florence/hikingblogrest/RouteController.java
+++ src/main/java/com/florence/hikingblogrest/RouteController.java
@@ -1,12 +1,14 @@
 package com.florence.hikingblogrest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RouteController {
 
-    @GetMapping("/getRoutes")
+    @GetMapping("/get-routes")
+    @CrossOrigin(origins="http://localhost:3000")
     public Routes getRoutes() {
         return new Routes();
     }
