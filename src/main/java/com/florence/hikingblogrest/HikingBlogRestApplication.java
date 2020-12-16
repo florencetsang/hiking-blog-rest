7b1d46c70acf7dd24697bef3f4f58106aa1a4c1b
florencetsang <florencesytsang@gmail.com>
2020-12-16 22:44:08 +0800

    Local database connection

--- src/main/java/com/florence/hikingblogrest/HikingBlogRestApplication.java
+++ src/main/java/com/florence/hikingblogrest/HikingBlogRestApplication.java
@@ -1,5 +1,6 @@
 package com.florence.hikingblogrest;
 
+import com.florence.hikingblogrest.proxy.DatabaseDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class HikingBlogRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HikingBlogRestApplication.class, args);
+
+//        DatabaseDAO database = new DatabaseDAO();
+//        database.getData();
     }
 
 }
