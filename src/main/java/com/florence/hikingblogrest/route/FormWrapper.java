27bd584e2c04e17bf1b358c8505f883dfc5243bf
florencetsang <florencesytsang@gmail.com>
2020-12-19 13:23:23 +0800

    Post request server for receiving create post form.

---
+++ src/main/java/com/florence/hikingblogrest/route/FormWrapper.java
@@ -0,0 +1,12 @@
+package com.florence.hikingblogrest.route;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FormWrapper {
+
+    private MultipartFile file;
+    private String name;
+    private String description;
+
+
+}
--- src/main/java/com/florence/hikingblogrest/route/RoutesController.java
+++ src/main/java/com/florence/hikingblogrest/route/RoutesController.java
@@ -1,13 +1,26 @@
 package com.florence.hikingblogrest.route;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 
 @RestController
 public class RoutesController {
+
+    private static final Logger LOGGER = LogManager.getLogger(RoutesController.class);
 
     RoutesService routesService;
 
@@ -20,4 +33,15 @@ public class RoutesController {
     public Routes getRoutes() throws FileNotFoundException {
         return routesService.getRoutes();
     }
+
+    @PostMapping(value = "/create-post", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5000"})
+    public ResponseEntity<String> createPost(HttpServletRequest request) throws IOException, ServletException {
+      Part filePart = request.getPart("file");
+        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+        LOGGER.info("Create post endpoint received {}. Name: {}, File: {}, Description: {}", request, request.getParameter("name"), fileName, request.getParameter("description"));
+        InputStream fileContent = filePart.getInputStream();
+        return ResponseEntity.ok("Received form data");
+    }
+
 }
