9d22f4fbf8a8663427bc89ef2e028f9b686480a8
florencetsang <florencesytsang@gmail.com>
2021-12-21 17:34:49 +0800

    Clean up.

--- src/main/java/com/florence/hikingblogrest/proxy/CloudStorageProxy.java
+++ src/main/java/com/florence/hikingblogrest/proxy/CloudStorageProxy.java
@@ -1,19 +1,15 @@
 package com.florence.hikingblogrest.proxy;
 
 import com.google.api.gax.paging.Page;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,28 +17,11 @@ import java.util.Map;
 public class CloudStorageProxy {
 
     private static final Logger LOGGER = LogManager.getLogger(CloudStorageProxy.class);
-    private static final String DATABASE_URL = "https://hiking-blog-app.firebaseio.com";
     private static final String PROJECT_ID = "hiking-blog-app";
     private static final String BUCKET_ID = "hiking-blog-app.appspot.com";
 
     public CloudStorageProxy() {
-//        initializeFirebase();
     }
-
-//    private void initializeFirebase() {
-//        try {
-//            FirebaseOptions options = FirebaseOptions.builder()
-//                    .setCredentials(GoogleCredentials.getApplicationDefault())
-//                    .setDatabaseUrl(DATABASE_URL)
-//                    .build();
-//
-//            FirebaseApp.initializeApp(options);
-//            LOGGER.info("Initialized Firebase App");
-//        } catch (IOException e) {
-//            LOGGER.error("ERROR: invalid service account credentials.");
-//            LOGGER.error(e.getMessage());
-//        }
-//    }
 
     public Map<String, InputStream> getAllGpxRoutes() {
 
