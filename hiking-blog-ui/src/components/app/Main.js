6cb558fc6b6b3593892dd1e0103454cfd5cf469f
florencetsang <florencesytsang@gmail.com>
2022-03-23 23:34:40 +0800

    Clean up.

--- hiking-blog-ui/src/components/app/Main.js
+++ hiking-blog-ui/src/components/app/Main.js
@@ -1,13 +1,7 @@
 import React from 'react';
-
-import Box from '@mui/material/Box';
-
 import { useSigninCheck } from 'reactfire';
-
 import Pages from '../header/Pages';
 import Footer from '../header/Footer';
-import AppMenu from '../header/AppMenu';
-
 import LoginPage from './LoginPage';
 
 export default function Main() {
