947b0250ef372ca57d66636267ac5389ff446e64
Man Chau <tsunman.chau.g@gmail.com>
2022-03-24 00:16:11 +0800

    HB-31 Limit file upload to GPX file only

---
+++ src/main/java/com/florence/hikingblogrest/utils/UploadFileAndValidator.kt
@@ -0,0 +1,10 @@
+package com.florence.hikingblogrest.utils
+
+import org.springframework.web.multipart.MultipartFile
+
+class UploadFileAndValidator(val validators: List<UploadFileValidator>): UploadFileValidator {
+    override fun validate(file: MultipartFile): Boolean {
+        return validators
+            .firstOrNull { validator -> !validator.validate(file) } == null
+    }
+}
---
+++ src/main/java/com/florence/hikingblogrest/utils/UploadFileExtensionValidator.kt
@@ -0,0 +1,17 @@
+package com.florence.hikingblogrest.utils
+
+import mu.KotlinLogging
+import org.springframework.web.multipart.MultipartFile
+
+class UploadFileExtensionValidator(val patterns: List<String>): UploadFileValidator {
+    private val logger = KotlinLogging.logger {}
+
+    override fun validate(file: MultipartFile): Boolean {
+        val fileName = file.originalFilename
+        if (!patterns.any { pattern -> fileName.endsWith(pattern) }) {
+            logger.info { "fileName $fileName does not match any of $patterns" }
+            return false
+        }
+        return true
+    }
+}
