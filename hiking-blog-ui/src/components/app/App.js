61ead437c4e2670d8a7e82d827a1b25742a49a4b
Man Chau <tsunman.chau.g@gmail.com>
2022-03-26 23:18:36 +0800

    HB-12 Set up Firebase emulator

--- hiking-blog-ui/src/components/app/App.js
+++ hiking-blog-ui/src/components/app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { getAuth } from 'firebase/auth';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 import { FirebaseAppProvider, useFirebaseApp, AuthProvider, AnalyticsProvider } from 'reactfire';
 import { SnackbarProvider } from 'notistack';
@@ -25,6 +25,14 @@ const _App = () => {
   const app = useFirebaseApp();
   const auth = getAuth(app);
   const analytics = getAnalytics(app);
+
+  if (process.env.NODE_ENV !== 'production' && process.env.REACT_APP_DEV_FIREBASE_EMULATOR === 'true') {
+    console.log('using Firebase emulator')
+    if (process.env.REACT_APP_DEV_FIREBASE_EMULATOR_AUTH_HOST) {
+      connectAuthEmulator(auth, process.env.REACT_APP_DEV_FIREBASE_EMULATOR_AUTH_HOST);
+      console.log('using auth emulator');
+    }
+  }
 
   return (
     <AuthProvider sdk={auth}>
